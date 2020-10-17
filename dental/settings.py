3f531e81650b33275a850b3b28b5ba764662ad8d
littleirishm <littleirishm@gmail.com>
2020-10-17 17:52:21 +0100

    change

--- dental/settings.py
+++ dental/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(x3$d&y(ma*6ns&#$jwqnuv$ju-4@l4+8^z7(u)g7baj)5j!k0'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
